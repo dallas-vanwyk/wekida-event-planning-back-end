37532945528e842e4ad6929debf2ec65dbd13893
Kier Roman <kierroman@gmail.com>
2025-04-15 19:09:55 -0500

    attendees schema created and functional

--- models/event.js
+++ models/event.js
@@ -1,58 +1,62 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+// models/event.js
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const eventSchema = new Schema({
+const attendeeSchema = new Schema({
+     name: {  
+        type: String,
+        required: true,
+     }
+})
+
+const eventSchema = new Schema(
+  {
     _id: {
-        type: Schema.Types.ObjectId,
-        default: () => new mongoose.Types.ObjectId(),
-        required: true
+      type: Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+      required: true,
     },
     event_title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     organizer: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    attendees: [{
-        _id: Schema.Types.ObjectId,
-        name: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-        }
-    }],
+    attendees: [attendeeSchema],
     category: {
-        type: String,
-        enum: ['weddings', 'sports', 'graduation party', 'baby shower', 'conference', 'arts & entertainment'],
-        required: true
+      type: String,
+      enum: ["Wedding", "Sports", "Graduation Party", "Baby Shower", "Conference", "Arts & Entertainment"],
+      required: true,
     },
     start_date: {
-        type: Date,
-        required: true
+      type: Date,
+      default: Date.now,
+//    required: true
     },
     end_date: {
-        type: Date,
-        required: true
+      type: Date,
+      default: Date.now,
+//    required: true
     },
     location: {
-        type: String
+      type: String,
     },
-}, {timestamps: true})
+  },
+  { timestamps: true }
+);
 
-const Event = mongoose.model('Event', eventSchema)
+const Event = mongoose.model("Event", eventSchema);
 
+module.exports = Event;
 
-module.exports = Event
 // venue_name: {
 //     type: String,
 //     required: true
@@ -70,34 +74,33 @@ module.exports = Event
 //     required: true
 // }
 
-
-    // const taskSchema = new Schema({
-    //     task_id: {
-    //         type: Schema.Types.ObjectId,
-    //         default: () => new mongoose.Types.ObjectId(),
-    //         required: true
-    //     },
-    //     task_name: {
-    //         type: String,
-    //         required: true
-    //     },
-    //     description: {
-    //         type: String,
-    //         required: true
-    //     },
-    //     due_date: {
-    //         type: Date,
-    //         required: true
-    //     },
-    //     assigned_to: {
-    //         type: Schema.Types.ObjectId,
-    //         ref: 'User',
-    //         required: true
-    //     },
-    //     status: {
-    //         type: String,
-    //         enum: ['pending', 'in-progress', 'completed'],
-    //         default: 'pending'
-    //     }
-    // }, {timestamps: true})
+// const taskSchema = new Schema({
+//     task_id: {
+//         type: Schema.Types.ObjectId,
+//         default: () => new mongoose.Types.ObjectId(),
+//         required: true
+//     },
+//     task_name: {
+//         type: String,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     due_date: {
+//         type: Date,
+//         required: true
+//     },
+//     assigned_to: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'User',
+//         required: true
+//     },
+//     status: {
+//         type: String,
+//         enum: ['pending', 'in-progress', 'completed'],
+//         default: 'pending'
+//     }
+// }, {timestamps: true})
 // Make guest Schema a child of the event schema that is just a list of creatable and deletable names

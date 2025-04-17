b6596aefeb2bc8c27302e1f46fb483c1b77e8351
Kier Roman <kierroman@gmail.com>
2025-04-17 17:09:09 -0500

    cleaned dead code off backend

--- models/event.js
+++ models/event.js
@@ -39,12 +39,10 @@ const eventSchema = new Schema(
     start_date: {
       type: Date,
       default: Date.now,
-//    required: true
     },
     end_date: {
       type: Date,
       default: Date.now,
-//    required: true
     },
     location: {
       type: String,
@@ -57,50 +55,3 @@ const Event = mongoose.model("Event", eventSchema);
 
 module.exports = Event;
 
-// venue_name: {
-//     type: String,
-//     required: true
-// },
-// address: {
-//     type: String,
-//     required: true
-// },
-// latitude: {
-//     type: Number,
-//     required: true
-// },
-// longitude: {
-//     type: Number,
-//     required: true
-// }
-
-// const taskSchema = new Schema({
-//     task_id: {
-//         type: Schema.Types.ObjectId,
-//         default: () => new mongoose.Types.ObjectId(),
-//         required: true
-//     },
-//     task_name: {
-//         type: String,
-//         required: true
-//     },
-//     description: {
-//         type: String,
-//         required: true
-//     },
-//     due_date: {
-//         type: Date,
-//         required: true
-//     },
-//     assigned_to: {
-//         type: Schema.Types.ObjectId,
-//         ref: 'User',
-//         required: true
-//     },
-//     status: {
-//         type: String,
-//         enum: ['pending', 'in-progress', 'completed'],
-//         default: 'pending'
-//     }
-// }, {timestamps: true})
-// Make guest Schema a child of the event schema that is just a list of creatable and deletable names
